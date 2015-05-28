78fccb618192682dffb003f3e9cb51b2f7b0008c
ShiChing <chnbohwr@gmail.com>
2015-05-28 22:21:51 +0800

    編輯飲料資料庫功能

--- nodejs/manage.js
+++ nodejs/manage.js
@@ -18,9 +18,6 @@ function loadDBSuccess() {
     company = lokidb.getCollection('company');
     menu = lokidb.getCollection('menu');
 
-    console.log('load shop items', shop.idIndex.length);
-    console.log('load company items', company.idIndex.length);
-    console.log('load menu items', menu.idIndex.length);
     if (shop === null) {
         shop = lokidb.addCollection('shop');
     }
@@ -30,6 +27,10 @@ function loadDBSuccess() {
     if (menu === null) {
         menu = lokidb.addCollection('menu');
     }
+
+    console.log('load shop items', shop.idIndex.length);
+    console.log('load company items', company.idIndex.length);
+    console.log('load menu items', menu.idIndex.length);
 
     //express listen 12340 port
     app.listen(12340);
@@ -71,19 +72,28 @@ app.post('/api/shops/', function (req, res) {
 });
 
 app.get('/api/company/menu/', function (req, res) {
-    var company_id = req.param('company_id');
-    var temp_menu = menu.find({
-        company_id: company_id
-    });
-    res.json(temp_menu)
+    var company_id = req.query['company_id'];
+    var menuid = company.get(company_id).menu;
+    if (!menuid) {
+        res.json({});
+    } else {
+        res.json(menu.get(menuid));
+    }
+
 });
 
 app.post('/api/company/menu/', function (req, res) {
     var temp_menu = req.body.menu;
     var company_id = req.body.company_id;
+
+    if (temp_menu.$loki) {
+        delete temp_menu.$loki;
+        delete temp_menu.meta;
+    }
+
     var db_menu = menu.insert(temp_menu);
-    var db_comapny = company.get(company_id);
-    if (db_comapny.menu_id) {
+    var db_company = company.get(company_id);
+    if (db_company.menu) {
         menu.remove(db_comapny.menu_id);
     }
     //指定id過去
--- nodejs/www/js/main.js
+++ nodejs/www/js/main.js
@@ -6,15 +6,63 @@ myapp.controller('mainController', function mainController($scope, service_drink
         $scope.companies = data;
     }
 
+    function successGetMenu(data) {
+        //如果有資料就放進去
+        if (data.list) {
+            $scope.menu = data;
+        } else {
+            $scope.menu = {
+                list: []
+            };
+        }
+        $scope.message = "取得資訊成功";
+    }
+
     $scope.$watch('selected', function (value) {
         if (value) {
-            service_drink.getShops(value).then(function (shops) {
-                $scope.shops = shops;
-                console.log(shops);
-            }, function () {})
+            $scope.message = "正在取得資訊";
+            service_drink.companyGetMenu(value).then(successGetMenu);
+            $scope.now_company_id = value;
         }
     });
 
+    $scope.createType = function () {
+        var default_type = {
+            name: '',
+            items: []
+        };
+
+        $scope.menu.list.push(default_type);
+    };
+
+    $scope.createItem = function (type) {
+        var item = {
+            name: '',
+            price_m: '25',
+            price_m: '30',
+            static_sugar: false,
+            hot: true,
+            onlyhot: false
+        };
+        type.items.push(item);
+    }
+
+    $scope.removeItem = function (index, type) {
+        type.items.splice(index, 1);
+    };
+
+    $scope.removeType = function (index) {
+        $scope.menu.list.splice(index, 1);
+    };
+
+    $scope.save = function () {
+        $scope.message = "正在存檔";
+        service_drink.saveMenu($scope.now_company_id, $scope.menu).then(function () {
+            $scope.message = "存檔成功";
+        });
+    };
+
+    window.scope = $scope;
 });
 
 myapp.service('service_drink', function service_drink($http, $q) {
@@ -63,4 +111,17 @@ myapp.service('service_drink', function service_drink($http, $q) {
 
         return defer.promise;
     };
+
+    this.saveMenu = function (company_id, menu) {
+        var defer = $q.defer();
+        $http.post('/api/company/menu', {
+            company_id: company_id,
+            menu: menu
+        }).success(function () {
+            defer.resolve();
+        }).error(function (e) {
+            defer.reject(e);
+        })
+        return defer.promise;
+    }
 });
