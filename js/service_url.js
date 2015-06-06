b50d9c9163c4453cd8f090d22eb6fc492c3753f9
ShiChing <chnbohwr@gmail.com>
2015-06-06 22:18:48 +0800

    整理程式碼 串連 service_url 跟 login http request

---
+++ js/service_url.js
@@ -0,0 +1,5 @@
+drinkapp.service('service_url', function () {
+    this.server_url = "http://127.0.0.1:8080"
+    this.login = this.server_url + '/login/';
+    this.signup = this.server_url + '/signup/';
+});
--- templates/login/login.js
+++ templates/login/login.js
@@ -1,11 +1,37 @@
-drinkapp.controller('login',function($scope){
-    
-    $scope.signup = function(){
+drinkapp.controller('login', function ($scope, $http, service_url) {
+    $scope.login_input = {
+        email: '',
+        password: ''
+    };
+    $scope.signup = function () {
         mainNavigator.pushPage('templates/login/signup.html');
     };
-    
-    $scope.login = function (){
-        mainNavigator.pushPage('templates/near/near.html');
+
+    $scope.login = function () {
+        if (!$scope.login_input.email || !$scope.login_input.password) {
+            alert('error, no email or password');
+        } else {
+            login();
+        }
     };
-    
+    /**
+     * @method login
+     * @param {Type}
+     */
+    function login() {
+        $http.post(service_url.login, {
+            email: $scope.email,
+            password: $scope.password
+        }).success(success).error(error);
+
+        function success(data) {
+            localStorage.token = data.token;
+        }
+
+        function error(e) {
+            console.log(e);
+        }
+    }
+
+    window.scope_login = $scope;
 });
