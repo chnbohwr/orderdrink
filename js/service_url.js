0610e1b1326a00d318886aa192aad3dd9b28fea6
chnbohwr <chnbohwr@gmail.com>
2015-06-10 20:25:01 +0800

    設定網址到測試的dns

--- js/service_url.js
+++ js/service_url.js
@@ -1,5 +1,5 @@
 drinkapp.service('service_url', function () {
-    this.server_url = "http://127.0.0.1:8080"
+    this.server_url = "http://orderdrink.ddns.net:14789"
     this.login = this.server_url + '/login/';
     this.signup = this.server_url + '/signup/';
     this.nearshop = this.server_url + '/api/location/';
