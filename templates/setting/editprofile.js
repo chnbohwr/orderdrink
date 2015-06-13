e0105a41daa85117dd131689e956d44efdf061a5
chnbohwr <chnbohwr@gmail.com>
2015-06-13 12:49:09 +0800

    準備要做修改個人資料的部分

--- templates/setting/editprofile.js
+++ templates/setting/editprofile.js
@@ -1,4 +1,8 @@
-drinkapp.controller('editprofile', function ($scope) {
+drinkapp.controller('editprofile', function ($scope,$http,service_url) {
     console.log('setting controller start');
+    $scope.name = localStorage.nickname;
     
+    $scope.changeName = function(){
+       //
+    };
 });
--- templates/setting/setting.js
+++ templates/setting/setting.js
@@ -1,5 +1,7 @@
 drinkapp.controller('setting', function ($scope) {
     console.log('setting controller start');
+    $scope.nickname = localStorage.nickname;
+    $scope.email = localStorage.email;
     $scope.gotoEditProfile = function(){
         mainNavigator.pushPage('templates/setting/editprofile.html');
     };
