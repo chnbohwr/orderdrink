f000b479efabd80782ccb5072aad7552276d69f3
young <y@dynamic.tw>
2015-07-24 11:18:49 +0800

    no message

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-var drinkapp = ons.bootstrap('drinkapp', ['onsen','angularMoment','checklist-model']);
+var drinkapp = ons.bootstrap('drinkapp', ['onsen', 'angularMoment', 'checklist-model', 'rn-lazy']);
 
-drinkapp.run(function(amMoment) {
+drinkapp.run(function (amMoment) {
     amMoment.changeLocale('zh-tw');
-    
+
 });
